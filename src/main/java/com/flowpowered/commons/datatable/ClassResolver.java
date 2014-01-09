d9a3e96dfc0ae132859f0cb895cca177112fc0f4
Wolf480pl <wolf480@interia.pl>
2014-01-09 20:24:32 +0100

    Fixed the license headers.

--- src/main/java/com/flowpowered/commons/datatable/ClassResolver.java
+++ src/main/java/com/flowpowered/commons/datatable/ClassResolver.java
@@ -1,7 +1,7 @@
 /*
- * This file is part of Spout LLC, licensed under the MIT License (MIT).
+ * This file is part of Flow Commons, licensed under the MIT License (MIT).
  *
- * Copyright (c) 2013 ${organization} <http://www.spout.org/>
+ * Copyright (c) 2013 Spout LLC <http://www.spout.org/>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
