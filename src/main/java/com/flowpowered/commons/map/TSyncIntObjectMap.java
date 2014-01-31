07aa57cdb9c6970fd282d1280787f1ed780bc0db
Jack Huey <kitskub@gmail.com>
2014-01-31 00:25:57 -0600

    Add TSyncIntObjectMap and TSyncIntObjectHashMap
    Gah special licensing

---
+++ src/main/java/com/flowpowered/commons/map/TSyncIntObjectMap.java
@@ -0,0 +1,42 @@
+/*
+ * This file is part of Flow Commons, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2013 Spout LLC <http://www.spout.org/>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.flowpowered.commons.map;
+
+import gnu.trove.map.TIntObjectMap;
+
+/**
+ * This is a synchronized version of the Trove TLongObjectMap
+ *
+ * @param <V> the value type
+ */
+public interface TSyncIntObjectMap<V> extends TIntObjectMap<V> {
+	/**
+	 * Removes a key/value pair from the map, but only if the key is mapped to a given value
+	 *
+	 * @param key the key
+	 * @param value the expected value
+	 * @return true if on success
+	 */
+	public boolean remove(int key, V value);
+}
